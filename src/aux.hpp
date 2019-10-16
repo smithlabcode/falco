f370dfd37c9e22504717bcaa4f90dc4e46250a8c
guilhermesena1 <desenabr@usc.edu>
2019-10-15 20:33:55 -0700

    Adding copyright to missing files

--- src/aux.hpp
+++ src/aux.hpp
@@ -1,3 +1,18 @@
+/* Copyright (C) 2019 Guilherme De Sena Brandine and
+ *                    Andrew D. Smith
+ * Authors: Guilherme De Sena Brandine, Andrew Smith
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
 #ifndef _AUX_HPP
 #define _AUX_HPP
 #include <string>
@@ -36,3 +51,4 @@ actg_to_2bit(const char &c) {
 }
 
 #endif
+
