5ffced5d09b8fe9d5782b3ca1f20bd0bf7be1686
guilhermesena1 <desenabr@usc.edu>
2019-10-16 20:36:29 -0700

    Fixing the falco run bash script

--- benchmark/run_all_falco_tests.sh
+++ benchmark/run_all_falco_tests.sh
@@ -3,7 +3,7 @@ for i in tests/fastq/test*.fastq
 do
   a=$(basename $i)
   echo $a
-  time falco -o outs/falco -i $i \
+  time falco -o outs/falco $i \
        1>outs/falco/output_${a} \
        2>outs/falco/error_${a}
 done
