65f4f8b4a77d556959eb955e346dd6ed2566fb2f
guilhermesena1 <desenabr@usc.edu>
2019-10-22 17:45:56 -0700

    module copyright

--- src/Module.hpp
+++ src/Module.hpp
@@ -1,3 +1,21 @@
+/* falco-diff : compare outputs of two fastqc/falco runs
+ *
+ * Copyright (C) 2019 Guilherme De Sena Brandine and
+ *                    Andrew D. Smith
+ * Authors: Guilherme De Sena Brandine, Andrew Smith
+ *
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ */
+
+
 #ifndef _MODULE_CPP
 #define _MODULE_CPP
 #include "FastqStats.hpp"
