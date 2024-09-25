cae18f47fd73f45eab101474e3c294c68ed6e92e
Andrew D Smith <andrewds@usc.edu>
2024-09-25 13:16:47 -0700

    changing the version number to v1.2.4

--- src/FalcoConfig.cpp
+++ src/FalcoConfig.cpp
@@ -35,7 +35,7 @@ using std::runtime_error;
 using std::istringstream;
 using std::cerr;
 
-const string FalcoConfig::FalcoVersion = "1.2.3";
+const string FalcoConfig::FalcoVersion = "1.2.4";
 
 /*********************************************************/
 /************** DEFAULT VALUES FOR FILES *****************/
