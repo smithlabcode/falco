7c455901486ec260e27c99c3d98d05fcf7c89129
Andrew D Smith <andrewds@usc.edu>
2024-10-23 16:19:43 -0700

    src/Module.cpp: Removed forgotten clang-format guards

--- src/Module.cpp
+++ src/Module.cpp
@@ -12,7 +12,7 @@
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * General Public License for more details.
  */
-// clang-format off
+
 #include "Module.hpp"
 #include <algorithm>
 #include <iomanip>
@@ -2194,4 +2194,3 @@ ModuleKmerContent::make_html_data() {
   }
   return data.str();
 }
-// clang-format on
