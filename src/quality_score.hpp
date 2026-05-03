441ccf5226097f09c0f01442c55307a21f2f12fe
Andrew D Smith <andrewds@usc.edu>
2026-05-02 20:44:22 -0700

    src/quality_score.hpp: added code to identify quality score encoding and the corresponding offset using the histogram of read counts by floor of mean quality

--- src/quality_score.hpp
+++ src/quality_score.hpp
@@ -24,9 +24,58 @@
 #ifndef SRC_QUALITY_SCORE_HPP_
 #define SRC_QUALITY_SCORE_HPP_
 
+#include <algorithm>
+#include <array>
+#include <format>
+#include <iterator>
+#include <ranges>
+#include <stdexcept>
+
 namespace falco {
-static constexpr auto max_qual_val = 128;
-static constexpr auto min_qual_val = 33;
+static constexpr auto max_qual_val = 126;
+// clang-format off
+static constexpr auto min_qual_cutoffs = std::array{
+  33,  // Sanger / Illumina 1.9
+  64,  // Solexa / Illumina 1.3 / Illumina 1.5
+};
+static constexpr auto min_qual_offsets = std::array{
+  0,
+  33,  // Sanger / Illumina 1.9
+  64,  // Solexa / Illumina 1.3 / Illumina 1.5
+};
+static constexpr auto format_labels = std::array{
+  "Unknown",
+  "Sanger / Illumina 1.9",
+  "Solexa / Illumina 1.3 / Illumina 1.5 / Illumina 1.8",
+};
+// clang-format on
 }  // namespace falco
+
+[[nodiscard]] inline auto
+identify_quality_score_idx(const auto &qual_counts) {
+  // get minimum quality score seen from an array of counts of observed scores
+  const auto gt0 = [](const auto c) { return c > 0; };
+  const auto min_qual_itr = std::ranges::find_if(qual_counts, gt0);
+  auto min_qual = std::distance(std::cbegin(qual_counts), min_qual_itr);
+  if (min_qual > falco::max_qual_val)
+    throw std::runtime_error(
+      std::format("found invalid quality score: {}", min_qual));
+  // now find the index among minimum values for formats
+  const auto itr = std::ranges::upper_bound(falco::min_qual_cutoffs, min_qual);
+  auto idx = std::distance(std::cbegin(falco::min_qual_cutoffs), itr);
+  if (idx == std::size(falco::min_qual_cutoffs))
+    --idx;
+  return idx;
+}
+
+[[nodiscard]] inline auto
+identify_quality_score_encoding(const auto &qual_counts) {
+  return falco::format_labels[identify_quality_score_idx(qual_counts)];
+}
+
+[[nodiscard]] inline auto
+identify_quality_score_offset(const auto &qual_counts) {
+  return falco::min_qual_offsets[identify_quality_score_idx(qual_counts)];
+}
 
 #endif  // SRC_QUALITY_SCORE_HPP_
