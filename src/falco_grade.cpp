4e621c252dbddb2da911e8df3054e97e44886935
Andrew D Smith <andrewds@usc.edu>
2026-06-19 18:58:38 -0700

    src/falco_grade.hcpp: adding sources for managing grades of analysis categories

---
+++ src/falco_grade.cpp
@@ -0,0 +1,61 @@
+/* MIT License
+ *
+ * Copyright (c) 2026 Andrew D Smith
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "falco_grade.hpp"
+
+#include <filesystem>
+#include <ranges>
+#include <stdexcept>
+#include <string>
+
+[[nodiscard]] auto
+analysis_grades::summary(const std::string &infile_path) const -> std::string {
+  const auto infile = std::filesystem::path(infile_path).filename().string();
+  const auto up = [](const auto &s) {
+    const auto u = [](const std::uint8_t c) { return std::toupper(c); };
+    return std::views::transform(s, u) | std::ranges::to<std::string>();
+  };
+  const auto compose = [&](const auto &g, const auto &lbl) {
+    static constexpr auto f = "{}\t{}\t{}\n";
+    return g.empty() ? std::string{} : std::format(f, up(g), lbl, infile);
+  };
+  std::string r;
+  for (const auto [idx, lbl] : std::views::enumerate(labels))
+    if (is_configured(lbl))
+      r += compose(grade(lbl), labels[idx]);
+  return r;
+}
+
+[[nodiscard]] auto
+analysis_grades::grade(const std::string &name) const -> std::string {
+  const auto itr = g.find(name);
+  return itr == std::cend(g) ? std::string{} : itr->second;
+}
+
+[[nodiscard]] auto
+analysis_grades::label(const std::string &name) const -> std::string {
+  const auto itr = std::ranges::find(names, name);
+  if (itr == std::cend(names))
+    throw std::runtime_error(std::format("bad grade category: {}", name));
+  return labels[std::distance(std::cbegin(names), itr)];
+}
---
+++ src/falco_grade.hpp
@@ -0,0 +1,101 @@
+/* MIT License
+ *
+ * Copyright (c) 2026 Andrew D Smith
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef SRC_FALCO_GRADE_HPP_
+#define SRC_FALCO_GRADE_HPP_
+
+#include <algorithm>
+#include <filesystem>
+#include <ranges>
+#include <string>
+#include <unordered_map>
+
+struct analysis_grades {
+  // clang-format off
+  static constexpr auto names = std::array{
+    "basic_stats",
+    "qual_by_pos",
+    "tiles",
+    "qual_by_read",
+    "base_comp",
+    "gc_content",
+    "n_content",
+    "read_lengths",
+    "duplication",
+    "overrep",
+    "adapters",
+    "kmers",
+  };
+  static constexpr auto labels = std::array{
+    "Basic Statistics",
+    "Per base sequence quality",
+    "Per tile sequence quality",
+    "Per sequence quality scores",
+    "Per base sequence content",
+    "Per sequence GC content",
+    "Per base N content",
+    "Sequence Length Distribution",
+    "Sequence Duplication Levels",
+    "Overrepresented sequences",
+    "Adapter Content",
+    "Kmer Content",
+  };
+  static_assert(std::size(names) == std::size(labels));
+  // clang-format on
+  std::unordered_map<std::string, std::string> g;
+
+  [[nodiscard]] static auto
+  is_valid(const std::string &name) -> bool {
+    return std::ranges::find(labels, name) != std::cend(labels);
+  }
+
+  [[nodiscard]] auto
+  is_configured(const std::string &name) const -> bool {
+    return g.contains(name);
+  }
+
+  auto
+  emplace(const std::string &name, const std::string &grade) {
+    g.emplace(name, grade);
+  }
+
+  [[nodiscard]] auto
+  grade(const std::string &name) const -> std::string;
+
+  [[nodiscard]] auto
+  label(const std::string &name) const -> std::string;
+
+  [[nodiscard]] auto
+  summary(const std::string &infile_path) const -> std::string;
+};
+
+[[nodiscard]] inline auto
+identify_grade(const auto &cutoffs, const auto c) {
+  const auto a = std::pair{c, std::string{}};
+  const auto b = std::ranges::lower_bound(cutoffs, a);
+  if (b == std::cend(cutoffs))
+    throw std::runtime_error("error in identifying grade");
+  return b->second;
+}
+
+#endif  // SRC_FALCO_GRADE_HPP_
