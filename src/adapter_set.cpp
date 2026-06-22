257061f6c4b46a6f07e99ff889634a5a2c22db51
Andrew D Smith <andrewds@usc.edu>
2026-06-22 15:07:18 -0700

    src/adapter_set.hcpp: adding sources to manage a set of adapters possibly provided by users, separate from the adapter matcher

---
+++ src/adapter_set.cpp
@@ -0,0 +1,110 @@
+/* MIT License
+ *
+ * Copyright (c) 2026 Andrew D Smith
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "adapter_set.hpp"
+
+#include "falco_utils.hpp"
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cstdint>
+#include <format>
+#include <fstream>
+#include <functional>
+#include <iterator>
+#include <numeric>
+#include <ranges>
+#include <string>
+#include <tuple>
+#include <vector>
+
+[[nodiscard]] static auto
+load_adapters(const std::string &filename) {
+  // ADS: (todo) handle carriage returns and other control chars
+  std::ifstream in(filename);
+  if (!in)
+    throw std::runtime_error("failed to open adapters file: " + filename);
+
+  std::vector<std::string> adapter_names;
+  std::vector<std::string> adapters;
+
+  std::string line_data;
+  while (std::getline(in, line_data)) {
+    std::string_view line = line_data;
+    const auto to_keep_prefix = line.find_first_not_of(" \t");
+    if (to_keep_prefix == std::string_view::npos)
+      continue;
+    line.remove_prefix(std::min(to_keep_prefix, std::size(line)));
+    if (line[0] == '#')
+      continue;
+    const auto to_keep_suffix = line.find_last_not_of(" \t");
+    if (to_keep_suffix == std::string_view::npos)
+      continue;
+    line.remove_suffix(std::size(line) - to_keep_suffix - 1);
+    std::string cleaned_line;
+    for (auto itr = std::cbegin(line); itr != std::cend(line); ++itr)
+      if (!std::isblank(*itr) ||
+          (std::next(itr) != std::cend(line) && *itr != *std::next(itr)))
+        cleaned_line += *itr;
+    const auto tab_pos = cleaned_line.find('\t');
+    if (tab_pos == std::string::npos ||
+        tab_pos != cleaned_line.find_last_of('\t'))
+      throw std::runtime_error("malformed line: " + line_data);
+    const auto is_print = [](const auto c) { return std::isprint(c); };
+    const auto name = cleaned_line.substr(0, tab_pos);
+    const auto seq = cleaned_line.substr(tab_pos + 1);
+    if (!std::ranges::all_of(name, is_print) ||
+        !std::ranges::all_of(seq, is_print))
+      throw std::runtime_error("malformed line: " + line_data);
+
+    adapter_names.push_back(name);
+    adapters.push_back(seq);
+  }
+  return std::tuple{adapter_names, adapters};
+}
+
+adapter_set::adapter_set(const std::string &filename) {
+  if (filename.empty()) {
+    std::ranges::copy(default_adapter_names, std::back_inserter(adapter_names));
+    std::ranges::copy(default_adapters, std::back_inserter(adapters));
+  }
+  else
+    std::tie(adapter_names, adapters) = load_adapters(filename);
+}
+
+[[nodiscard]] auto
+adapter_set::validate() const -> std::tuple<bool, std::string> {
+  const auto adap_len = std::size(instance().adapters.front());
+  if (!std::ranges::all_of(instance().adapters, [&](const auto &a) {
+        return std::size(a) == adap_len;
+      }))
+    return {false, "adapter patterns must have equal length"};
+  if (adap_len < min_adapter_size)
+    return {false, std::format("adapter patterns must have length at least {}",
+                               min_adapter_size)};
+  if (adap_len > max_adapter_size)
+    return {false, std::format("adapter patterns must have length at most {}",
+                               max_adapter_size)};
+  return {true, std::string{}};
+}
---
+++ src/adapter_set.hpp
@@ -0,0 +1,93 @@
+/* MIT License
+ *
+ * Copyright (c) 2026 Andrew D Smith
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef SRC_ADAPTER_SET_HPP_
+#define SRC_ADAPTER_SET_HPP_
+
+#include "falco_utils.hpp"
+
+#include <array>
+#include <cstdint>
+#include <format>  // IWYU pragma: keep
+#include <iterator>
+#include <limits>
+#include <string>
+#include <thread>  // IWYU pragma: keep
+#include <utility>
+#include <vector>
+
+struct adapter_set {
+  static constexpr auto min_adapter_size = 6;
+  static constexpr auto max_adapter_size = 16;
+
+  static constexpr auto default_n_adapters = 6;
+  static constexpr auto default_adapter_size = 12;
+  static constexpr auto default_adapter_names = std::array{
+    // clang-format off
+    "Illumina Universal Adapter",
+    "Illumina Small RNA 3' Adapter",
+    "Illumina Small RNA 5' Adapter",
+    "Nextera Transposase Sequence",
+    "PolyA",
+    "PolyG",
+    // clang-format on
+  };
+  static constexpr auto default_adapters = std::array{
+    // clang-format off
+    "AGATCGGAAGAG",  // Illumina Universal Adapter
+    "TGGAATTCTCGG",  // Illumina Small RNA 3' Adapter
+    "GATCGTCGGACT",  // Illumina Small RNA 5' Adapter
+    "CTGTCTCTTATA",  // Nextera Transposase Sequence
+    "AAAAAAAAAAAA",  // PolyA
+    "GGGGGGGGGGGG",  // PolyG
+    // clang-format on
+  };
+
+  std::vector<std::string> adapter_names;
+  std::vector<std::string> adapters;
+
+  // clang-format off
+  [[nodiscard]] auto size() const { return std::size(adapters); }
+  [[nodiscard]] auto ssize() const { return std::ssize(adapters); }
+  // clang-format on
+
+  static auto
+  instance(const std::string &filename = std::string{}) -> const adapter_set & {
+    static const adapter_set s(filename);
+    return s;
+  }
+
+  [[nodiscard]] auto
+  validate() const -> std::tuple<bool, std::string>;
+
+  adapter_set(const adapter_set &) = delete;
+  auto
+  operator=(const adapter_set &) -> adapter_set & = delete;
+
+private:
+  adapter_set(const std::string &filename);
+  adapter_set() = default;
+  ~adapter_set() = default;
+};  // adapter_set
+
+#endif  // SRC_ADAPTER_SET_HPP_
