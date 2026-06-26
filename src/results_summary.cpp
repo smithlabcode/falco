d95231b03dd9f70f2e858735755e87e969118a6c
Andrew D Smith <andrewds@usc.edu>
2026-06-25 18:51:28 -0700

    src/results_summary.hcpp: adding class to handle post-analysis summary work to keep it out of the classes that hold data during analysis

---
+++ src/results_summary.cpp
@@ -0,0 +1,161 @@
+/* MIT License
+ *
+ * Copyright (c) 2026 Andrew D Smith
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#include "results_summary.hpp"
+
+#include "falco_utils.hpp"
+#include "html.hpp"
+#include "report.hpp"
+#include "tile_processor.hpp"
+
+#include <algorithm>
+#include <cstdint>
+#include <iterator>
+#include <ranges>
+#include <string>
+#include <thread>
+#include <type_traits>
+#include <unordered_map>
+#include <vector>
+
+auto
+results_summary::initialize() -> void {
+  // assign scalar variables
+  const auto gt0 = [](const auto c) { return c > 0; };
+  min_read_len = std::ranges::distance(std::cbegin(lengths),
+                                       std::ranges::find_if(lengths, gt0));
+  total_bases = tabular_dot(lengths);
+  const auto gc_acc = [](const auto a, const auto &nuc) {
+    return a + nuc[guanine_index] + nuc[cytosine_index];
+  };
+  total_gc = std::accumulate(std::cbegin(base_counts), std::cend(base_counts),
+                             0ul, gc_acc);
+
+  // get summary structures
+  centered = tp.get_centered();
+  kmer_results = kc.get_kmer_results();
+
+  // apply groups
+  groups = get_default_base_groups(max_read_len, do_groups(mode));
+  if (do_groups(mode)) {
+    apply_base_groups(groups, base_counts);
+    apply_base_groups(groups, n_counts);
+    // ADS: need one for lengths
+    apply_base_groups(groups, qual_by_pos);
+  }
+
+  // assign grades
+  assign_grades();
+}
+
+auto
+results_summary::assign_grades() -> void {
+  grades.emplace("basic_stats", get_grade_basic_stats());
+  grades.emplace("quality_base", get_grade_quality_base(qual_by_pos));
+  grades.emplace("quality_sequence", get_grade_quality_sequence(qual_by_read));
+  grades.emplace("sequence", get_grade_sequence(base_counts));
+  grades.emplace("gc_sequence", get_grade_gc_sequence(gc_content));
+  grades.emplace("n_content", get_grade_n_content(n_counts, base_counts));
+  grades.emplace("sequence_length", get_grade_sequence_length(lengths));
+  grades.emplace("duplication", dr.get_grade_duplication());
+  grades.emplace("overrepresented", dr.get_grade_overrepresented());
+  grades.emplace("adapter", am.get_grade(n_reads));
+  if (do_tiles(mode))
+    grades.emplace("tile", get_grade_tile(centered));
+  if (do_kmers(mode))
+    grades.emplace("kmer", get_grade_kmer(kmer_results));
+}
+
+[[nodiscard]] auto
+results_summary::get_report() const -> std::string {
+  const auto basic_stats = basic_stats_report(
+    info, n_reads, min_read_len, max_read_len, total_gc, total_bases, grades);
+  auto sections = std::unordered_map<std::string, std::string>{
+    {"basic_stats", basic_stats},
+    {"quality_base", quality_base_report(qual_by_pos, groups, grades)},
+    {"quality_sequence", quality_sequence_report(qual_by_read, grades)},
+    {"sequence", sequence_report(base_counts, groups, grades)},
+    {"gc_sequence", gc_sequence_report(gc_content, grades)},
+    {"n_content", n_content_report(n_counts, base_counts, groups, grades)},
+    {"sequence_length", sequence_length_report(lengths, grades)},
+    {"duplication", dr.duplication_report(grades)},
+    {"overrepresented", dr.overrepresented_report(grades)},
+    {"adapter", am.report(n_reads, groups, grades)},
+  };
+  if (do_tiles(mode)) {
+    assert(!centered.empty());
+    sections.emplace("tile", tile_report(centered, groups, grades));
+  }
+  if (do_kmers(mode)) {
+    assert(!kmer_results.empty());
+    sections.emplace("kmer", kmer_report(kmer_results, grades));
+  }
+  std::string report;
+  for (const auto &name : section_names)
+    if (const auto itr = sections.find(name); itr != std::cend(sections))
+      report += itr->second;
+  return report;
+}
+
+auto
+results_summary::apply_groups(
+  [[maybe_unused]] const std::vector<base_group_t> &groups) -> void {
+  return;
+}
+
+[[nodiscard]] auto
+results_summary::get_html() const -> std::string {
+  const auto basic_stats = basic_stats_html(
+    info, n_reads, min_read_len, max_read_len, total_gc, total_bases, grades);
+  auto sections = std::unordered_map<std::string, std::string>{
+    {"basic_stats", basic_stats},
+    {"quality_base", quality_base_html(qual_by_pos, groups, grades)},
+    {"quality_sequence", quality_sequence_html(qual_by_read, grades)},
+    {"sequence", sequence_html(base_counts, groups, grades)},
+    {"gc_sequence", gc_sequence_html(gc_content, grades)},
+    {"n_content", n_content_html(n_counts, base_counts, groups, grades)},
+    {"sequence_length", sequence_length_html(lengths, grades)},
+    {"duplication", dr.duplication_html(grades)},
+    {"overrepresented", dr.overrepresented_html(grades)},
+    {"adapter", am.html(n_reads, groups, grades)},
+  };
+  if (do_tiles(mode)) {
+    assert(!centered.empty());
+    sections.emplace("tile", tile_html(centered, groups, grades));
+  }
+  if (do_kmers(mode)) {
+    assert(!kmer_results.empty());
+    sections.emplace("kmer", kmer_html(kmer_results, grades));
+  }
+
+  assert(std::ranges::all_of(
+    sections,  // make sure all grades are configured
+    [&](const auto &s) { return grades.is_configured(s); },
+    [&](const auto &s) { return s.first; }));
+
+  std::string modules;
+  for (const auto &name : section_names)
+    if (const auto itr = sections.find(name); itr != std::cend(sections))
+      modules += itr->second;
+  return falco_get_html(info, grades, modules);
+}
---
+++ src/results_summary.hpp
@@ -0,0 +1,157 @@
+/* MIT License
+ *
+ * Copyright (c) 2026 Andrew D Smith
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+#ifndef SRC_RESULTS_SUMMARY_HPP_
+#define SRC_RESULTS_SUMMARY_HPP_
+
+#include "results_collector.hpp"
+
+#include "adapter_matcher.hpp"
+#include "bam_file.hpp"
+#include "duplication_results.hpp"
+#include "falco_file_format.hpp"
+#include "falco_grade.hpp"
+#include "falco_utils.hpp"
+#include "fastq_file.hpp"
+#include "format_output.hpp"
+#include "kmer_counter.hpp"
+#include "tile_processor.hpp"
+
+#include <algorithm>
+#include <cstdint>
+#include <ranges>
+#include <string>
+#include <type_traits>
+#include <unordered_map>
+#include <vector>
+
+struct results_summary {
+  std::uint64_t n_reads{};
+  std::uint64_t min_read_len{};
+  std::uint64_t max_read_len{};
+  std::uint64_t total_bases{};
+  std::uint64_t total_gc{};
+  std::vector<falco::nuc_array> base_counts;
+  falco::gc_content_array gc_content{};
+  std::vector<std::uint64_t> n_counts;
+  std::vector<std::uint64_t> lengths;
+  std::vector<falco::qual_array> qual_by_pos;
+  falco::qual_array qual_by_read{};
+  duplication_results dr;
+  adapter_matcher am;
+  tile_processor tp;
+  tile_processor::tiles_centered_t centered;
+  kmer_counter kc;
+  std::vector<kmer_result> kmer_results;
+  file_grades grades;
+  run_mode mode;
+  file_info info;
+  std::vector<base_group_t> groups;
+
+  results_summary(results_collector &rc, const run_mode &mode,
+                  const file_info &info) : mode{mode}, info{info} {
+    n_reads = rc.n_reads;
+    max_read_len = rc.max_read_len;
+    base_counts = std::move(rc.base_counts);
+    gc_content = std::move(rc.gc_content);
+    n_counts = std::move(rc.n_counts);
+    lengths = std::move(rc.lengths);
+    qual_by_pos = std::move(rc.qual_by_pos);
+    qual_by_read = std::move(rc.qual_by_read);
+    dr = std::move(rc.dr);
+    am = std::move(rc.am);
+    initialize();
+  }
+
+  results_summary(results_collector_tile &rc, const run_mode &mode,
+                  const file_info &info) : mode{mode}, info{info} {
+    n_reads = rc.n_reads;
+    max_read_len = rc.max_read_len;
+    base_counts = std::move(rc.base_counts);
+    gc_content = std::move(rc.gc_content);
+    n_counts = std::move(rc.n_counts);
+    lengths = std::move(rc.lengths);
+    qual_by_pos = std::move(rc.qual_by_pos);
+    qual_by_read = std::move(rc.qual_by_read);
+    dr = std::move(rc.dr);
+    am = std::move(rc.am);
+    //
+    tp = std::move(rc.tp);
+
+    initialize();
+  }
+
+  results_summary(results_collector_kmer &rc, const run_mode &mode,
+                  const file_info &info) : mode{mode}, info{info} {
+    n_reads = rc.n_reads;
+    max_read_len = rc.max_read_len;
+    base_counts = std::move(rc.base_counts);
+    gc_content = std::move(rc.gc_content);
+    n_counts = std::move(rc.n_counts);
+    lengths = std::move(rc.lengths);
+    qual_by_pos = std::move(rc.qual_by_pos);
+    qual_by_read = std::move(rc.qual_by_read);
+    dr = std::move(rc.dr);
+    am = std::move(rc.am);
+    //
+    kc = std::move(rc.kc);
+
+    initialize();
+  }
+
+  results_summary(results_collector_tile_kmer &rc, const run_mode &mode,
+                  const file_info &info) : mode{mode}, info{info} {
+    n_reads = rc.n_reads;
+    max_read_len = rc.max_read_len;
+    base_counts = std::move(rc.base_counts);
+    gc_content = std::move(rc.gc_content);
+    n_counts = std::move(rc.n_counts);
+    lengths = std::move(rc.lengths);
+    qual_by_pos = std::move(rc.qual_by_pos);
+    qual_by_read = std::move(rc.qual_by_read);
+    dr = std::move(rc.dr);
+    am = std::move(rc.am);
+    //
+    tp = std::move(rc.tp);
+    kc = std::move(rc.kc);
+
+    initialize();
+  }
+
+  auto
+  apply_groups(const std::vector<base_group_t> &groups) -> void;
+
+  auto
+  assign_grades() -> void;
+
+  auto
+  initialize() -> void;
+
+  [[nodiscard]] auto
+  get_report() const -> std::string;
+
+  [[nodiscard]] auto
+  get_html() const -> std::string;
+};
+
+#endif  // SRC_RESULTS_SUMMARY_HPP_
