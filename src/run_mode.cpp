7af9245432447143c65a3008753968c3e78d817c
Andrew D Smith <andrewds@usc.edu>
2026-06-29 16:04:29 -0700

    src/run_mode.hcpp: adding all the run_mode vars and functions to fully control output and processing

--- src/run_mode.cpp
+++ src/run_mode.cpp
@@ -29,19 +29,43 @@
 #include <string>
 #include <vector>
 
+// ADS: mapping between labels and function/var names
+
+// clang-format off
+//
+// Label: what appears in config file
+// var/fun name: how members of run_mode class are named
+// Affect processing: if yes, then changes how analysis is done
+//
+// Label            | var/fun name  | affect processing
+// ------------------------------------------------------------
+// adapter          | do_adap       | Yes
+// duplication      | do_dups       | Yes
+// gc_sequence      | do_gc_content |
+// kmer             | do_kmers      | Yes
+// n_content        | do_n_content  |
+// overrepresented  | do_overrep    | Yes (equivalent to dups)
+// quality_base     | do_qual_base  |
+// quality_sequence | do_qual_seq   |
+// sequence         | do_sequence   |
+// sequence_length  | do_length     |
+// tile             | do_tiles      | Yes
+// ------------------------------------------------------------
+// clang-format on
+
 // clang-format off
 std::vector<std::string> run_mode::labels{  // NOLINT(cert-err58-cpp)
+  "adapter",
   "duplication",
+  "gc_sequence",
   "kmer",
   "n_content",
   "overrepresented",
   "quality_base",
-  "sequence",
-  "gc_sequence",
   "quality_sequence",
-  "tile",
+  "sequence",
   "sequence_length",
-  "adapter",
+  "tile",
 };
 // clang-format on
 
@@ -53,22 +77,34 @@ run_mode::assign(const boost::unordered_flat_map<std::string, bool> &modes)
     if (itr != std::cend(modes))
       the_mode = itr->second ? 1 : -1;
   };
-  set_mode("tile", do_tiles_);
-  set_mode("kmer", do_kmers_);
-  set_mode("duplication", do_dups_);
   set_mode("adapter", do_adap_);
+  set_mode("duplication", do_dups_);
+  set_mode("gc_sequence", do_gc_content_);
+  set_mode("kmer", do_kmers_);
+  set_mode("sequence_length", do_length_);
+  set_mode("n_content", do_n_content_);
+  set_mode("overrepresented", do_overrep_);
+  set_mode("quality_base", do_qual_base_);
+  set_mode("quality_sequence", do_qual_seq_);
+  set_mode("sequence", do_sequence_);
+  set_mode("tile", do_tiles_);
 }
 
 auto
 run_mode::set_unassigned() -> void {
-  if (do_tiles_ == 0)
-    do_tiles_ = do_tiles_default;
-  if (do_kmers_ == 0)
-    do_kmers_ = do_kmers_default;
-  if (do_dups_ == 0)
-    do_dups_ = do_dups_default;
-  if (do_adap_ == 0)
-    do_adap_ = do_adap_default;
-  if (do_groups_ == 0)
-    do_groups_ = do_groups_default;
+  // clang-format off
+  if (do_adap_ == 0) do_adap_ = do_adap_default;
+  if (do_dups_ == 0) do_dups_ = do_dups_default;
+  if (do_gc_content_ == 0) do_gc_content_ = do_gc_content_default;
+  if (do_kmers_ == 0) do_kmers_ = do_kmers_default;
+  if (do_n_content_ == 0) do_n_content_ = do_n_content_default;
+  if (do_overrep_ == 0) do_overrep_ = do_overrep_default;
+  if (do_qual_base_ == 0) do_qual_base_ = do_qual_base_default;
+  if (do_qual_seq_ == 0) do_qual_seq_ = do_qual_seq_default;
+  if (do_sequence_ == 0) do_sequence_ = do_sequence_default;
+  if (do_length_ == 0) do_length_ = do_length_default;
+  if (do_tiles_ == 0) do_tiles_ = do_tiles_default;
+  // 'groups' not set in config file
+  if (do_groups_ == 0) do_groups_ = do_groups_default;
+  // clang-format on
 }
--- src/run_mode.hpp
+++ src/run_mode.hpp
@@ -40,42 +40,77 @@ public:
   }
 
   // clang-format off
-  [[nodiscard]] auto do_tiles() const -> bool { return do_tiles_ == 1; }
-  [[nodiscard]] auto do_dups() const -> bool { return do_dups_ == 1; }
-  [[nodiscard]] auto do_kmers() const -> bool { return do_kmers_ == 1; }
-  [[nodiscard]] auto do_adap() const -> bool { return do_adap_ == 1; }
+  // ADS: do_groups is not set in config file
   [[nodiscard]] auto do_groups() const -> bool { return do_groups_ == 1; }
+  //
+  [[nodiscard]] auto do_adap() const -> bool { return do_adap_ == 1; }
+  [[nodiscard]] auto do_dups() const -> bool { return do_dups_ == 1; }
+  [[nodiscard]] auto do_gc_content() const -> bool { return do_gc_content_ == 1; }
+  [[nodiscard]] auto do_kmers() const -> bool { return do_kmers_ == 1; }
+  [[nodiscard]] auto do_length() const -> bool { return do_length_ == 1; }
+  [[nodiscard]] auto do_n_content() const -> bool { return do_n_content_ == 1; }
+  [[nodiscard]] auto do_overrep() const -> bool { return do_overrep_ == 1; }
+  [[nodiscard]] auto do_qual_base() const -> bool { return do_qual_base_ == 1; }
+  [[nodiscard]] auto do_qual_seq() const -> bool { return do_qual_seq_ == 1; }
+  [[nodiscard]] auto do_sequence() const -> bool { return do_sequence_ == 1; }
+  [[nodiscard]] auto do_tiles() const -> bool { return do_tiles_ == 1; }
+  // ADS: below, run duplication analysis if either dups or overrep is requested
+  [[nodiscard]] auto do_dup_analysis() const -> bool {
+    return do_overrep() || do_dups();
+  }
   // clang-format on
 
   // clang-format off
-  auto set_do_tiles(const int x) { if (x) do_tiles_ = x; }
-  auto set_do_dups(const int x) { if (x) do_dups_ = x; }
-  auto set_do_kmers(const int x) { if (x) do_kmers_ = x; }
-  auto set_do_adap(const int x) { if (x) do_adap_ = x; }
   auto set_do_groups(const int x) { if (x) do_groups_ = x; }
-  // clang-format on
+  //
+  auto set_do_adap(const int x) { if (x) do_adap_ = x; }
+  auto set_do_dups(const int x) { if (x) do_dups_ = x; }
+  auto set_do_gc_content(const int x) { if (x) do_gc_content_ = x; }
+  auto set_do_kmers(const int x) { if (x) do_kmers_ = x; }
+  auto set_do_length(const int x) { if (x) do_length_ = x; }
+  auto set_do_n_content(const int x) { if (x) do_n_content_ = x; }
+  auto set_do_overrep(const int x) { if (x) do_overrep_ = x; }
+  auto set_do_qual_base(const int x) { if (x) do_qual_base_ = x; }
+  auto set_do_qual_seq(const int x) { if (x) do_qual_seq_ = x; }
+  auto set_do_sequence(const int x) { if (x) do_sequence_ = x; }
+  auto set_do_tiles(const int x) { if (x) do_tiles_ = x; }
 
   [[nodiscard]] static auto
-  get_labels() -> const std::vector<std::string> & {
-    return labels;
-  }
+  get_labels() -> const std::vector<std::string> & { return labels; }
+  // clang-format on
 
   auto
   assign(const boost::unordered_flat_map<std::string, bool> &modes) -> void;
 
 private:
   // ADS: 1 is yes; -1 is no; 0 is not assigned
-  static constexpr auto do_tiles_default = -1;
-  static constexpr auto do_kmers_default = -1;
-  static constexpr auto do_groups_default = -1;
-  static constexpr auto do_dups_default = 1;
-  static constexpr auto do_adap_default = 1;
+  static constexpr auto do_groups_default = -1;  // OFF
 
-  int do_tiles_{};
-  int do_kmers_{};
-  int do_dups_{};
-  int do_adap_{};
+  static constexpr auto do_adap_default = 1;  // affects processing
+  static constexpr auto do_dups_default = 1;  // affects processing
+  static constexpr auto do_gc_content_default = 1;
+  static constexpr auto do_kmers_default = -1;  // OFF affects processing
+  static constexpr auto do_length_default = 1;
+  static constexpr auto do_n_content_default = 1;
+  static constexpr auto do_overrep_default = 1;  // affects processing
+  static constexpr auto do_qual_base_default = 1;
+  static constexpr auto do_qual_seq_default = 1;
+  static constexpr auto do_sequence_default = 1;
+  static constexpr auto do_tiles_default = -1;  // OFF affects processing
+
   int do_groups_{};
+
+  int do_adap_{};
+  int do_dups_{};
+  int do_gc_content_{};
+  int do_kmers_{};
+  int do_length_{};
+  int do_n_content_{};
+  int do_overrep_{};
+  int do_qual_base_{};
+  int do_qual_seq_{};
+  int do_sequence_{};
+  int do_tiles_{};
 
   static std::vector<std::string> labels;
 };
