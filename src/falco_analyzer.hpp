baf8550b6f6d1b0d79047ae96bd99c46c1db14cd
Andrew D Smith <andrewds@usc.edu>
2026-07-08 13:25:23 -0700

    src/falco_analyzer.hpp: modifying task queue so tasks can be specific to bamrec or fqrec which allows both types of files to be analyzed at the same time. This requires some setup including a concept class for type erasure.

--- src/falco_analyzer.hpp
+++ src/falco_analyzer.hpp
@@ -47,14 +47,110 @@
 #include <utility>
 #include <vector>
 
-template <typename results_t, typename rec_t> struct analyzer_t {
-  static constexpr auto n_chunks_per_thread = 8;
-  using pos_t = typename rec_t::pos_t;
-  struct task_t {
-    std::int64_t file_id{};
-    pos_t beg{};
-    pos_t end{};
+struct fq_task_t {
+  std::int64_t file_id{};
+  fqrec::pos_t beg{};
+  fqrec::pos_t end{};
+};
+struct bam_task_t {
+  std::int64_t file_id{};
+  bamrec::pos_t beg{};
+  bamrec::pos_t end{};
+};
+
+using task_t = std::variant<fq_task_t, bam_task_t>;
+
+static auto
+make_tasks_impl(const std::int64_t file_id,
+                const bam_chunks_t &chunks) -> std::vector<task_t> {
+  std::vector<task_t> tasks;
+  tasks.reserve(std::size(chunks));
+  for (const auto &chunk : chunks)
+    tasks.push_back(bam_task_t(file_id, chunk.first, chunk.second));
+  return tasks;
+}
+
+[[nodiscard]] static auto
+make_tasks_impl(const std::int64_t file_id,
+                const fq_chunks_t &chunks) -> std::vector<task_t> {
+  std::vector<task_t> tasks;
+  tasks.reserve(std::size(chunks));
+  for (const auto &chunk : chunks)
+    tasks.push_back(fq_task_t(file_id, chunk.first, chunk.second));
+  return tasks;
+}
+
+template <typename T>
+concept concrete_reads_file_t =        //
+  std::same_as<T, fastq_file> ||       //
+  std::same_as<T, fastq_bgzf_file> ||  //
+  std::same_as<T, fastq_gz_file> ||    //
+  std::same_as<T, bam_file>;
+
+[[nodiscard]] inline auto
+make_tasks(concrete_reads_file_t auto &reads_file, const std::int64_t file_id,
+           const std::int64_t n_chunks) -> std::vector<task_t> {
+  reads_file.load_next();
+  const auto chunks = reads_file.get_chunks(n_chunks);
+  return make_tasks_impl(file_id, chunks);
+}
+
+inline auto
+load_next(concrete_reads_file_t auto &f) -> void {
+  f.load_next();
+}
+
+[[nodiscard]] inline auto
+is_active(const concrete_reads_file_t auto &reads_file) -> bool {
+  return static_cast<bool>(reads_file);
+}
+
+class reads_file_t {
+public:
+  template <typename T> reads_file_t(T x) : self_(new model<T>(std::move(x))) {}
+  reads_file_t(const reads_file_t &x) = delete;
+  auto
+  operator=(const reads_file_t &x) -> reads_file_t & = delete;
+  auto
+  operator=(reads_file_t &&) noexcept -> reads_file_t & = delete;
+  reads_file_t(reads_file_t &&) noexcept = default;
+  [[nodiscard]] friend auto
+  is_active(const reads_file_t &x) -> bool {
+    return x.self_->is_active_();
+  }
+  [[nodiscard]] friend auto
+  make_tasks(auto &reads_file, const std::int64_t file_id,
+             const std::int64_t n_chunks) -> std::vector<task_t> {
+    return reads_file.self_->make_tasks_(file_id, n_chunks);
+  }
+
+private:
+  struct concept_t {
+    virtual ~concept_t() = default;
+    virtual auto
+    make_tasks_(const std::int64_t,
+                const std::int64_t) -> std::vector<task_t> = 0;
+    virtual auto
+    is_active_() const -> bool = 0;
   };
+  template <typename T> struct model : concept_t {
+    model(T x) : data_(std::move(x)) {}
+    [[nodiscard]] auto
+    make_tasks_(const std::int64_t file_id,
+                const std::int64_t n_chunks) -> std::vector<task_t> override {
+      return make_tasks(data_, file_id, n_chunks);
+    }
+    [[nodiscard]] auto
+    is_active_() const -> bool override {
+      return is_active(data_);
+    }
+    T data_;
+  };
+  std::unique_ptr<concept_t> self_;
+};
+
+template <typename results_t> struct analyzer_t {
+  static constexpr auto n_chunks_per_thread = 8;
   std::mutex task_queue_mtx;
   std::condition_variable_any task_queue_cv;
   std::queue<task_t> task_queue;
@@ -82,6 +178,7 @@ template <typename results_t, typename rec_t> struct analyzer_t {
       for (const auto [file_id, info] : std::views::enumerate(infos))
         res[file_id].init(mode, info);
 
+    // put each file in the file queue to start
     for (const auto f_id : std::views::iota(0u, n_files))
       file_queue.emplace(f_id);
 
@@ -97,10 +194,20 @@ template <typename results_t, typename rec_t> struct analyzer_t {
                                [this] { return !task_queue.empty(); });
             if (stop.stop_requested() && task_queue.empty())
               return;
-            const auto task = pop_task(task_queue_lock);
-            res[task.file_id].template process_reads<rec_t>(task.beg, task.end);
-            if (n_tasks[task.file_id].fetch_sub(1) == 1)
-              push_file(task.file_id);
+            const auto task = pop_task_and_release_lock(task_queue_lock);
+            auto file_id = 0L;
+            if (std::holds_alternative<fq_task_t>(task)) {
+              const auto fq_task = std::get<fq_task_t>(task);
+              file_id = fq_task.file_id;
+              res[file_id].process_reads_fq(fq_task.beg, fq_task.end);
+            }
+            else {  // std::holds_alternative<bam_task_t>(task)
+              const auto bam_task = std::get<bam_task_t>(task);
+              file_id = bam_task.file_id;
+              res[file_id].process_reads_bam(bam_task.beg, bam_task.end);
+            }
+            if (n_tasks[file_id].fetch_sub(1) == 1)
+              push_file(file_id);
           }
         });
 
@@ -115,20 +222,18 @@ template <typename results_t, typename rec_t> struct analyzer_t {
             });
             if (n_active_files == 0)
               return;
-            const auto f_id = pop_file(file_queue_lock);
-            if (!reads_files[f_id]) {
+            const auto file_id = pop_file_and_release_lock(file_queue_lock);
+            if (!is_active(reads_files[file_id])) {
               if (--n_active_files == 0) {
                 file_queue_cv.notify_all();
                 return;
               }
               continue;
             }
-            reads_files[f_id].load_next();
-            const auto chunks =
-              get_chunks(reads_files[f_id], n_chunks_per_file);
-            n_tasks[f_id] = static_cast<std::int32_t>(std::ssize(chunks));
-            std::ranges::for_each(chunks,
-                                  [&](const auto &c) { push_task(f_id, c); });
+            const auto tasks =
+              make_tasks(reads_files[file_id], file_id, n_chunks_per_file);
+            n_tasks[file_id] = static_cast<std::int32_t>(std::ssize(tasks));
+            std::ranges::for_each(tasks, [&](const auto &c) { push_task(c); });
           }
         });
       std::ranges::for_each(readers, [](auto &reader) { reader.join(); });
@@ -142,15 +247,15 @@ template <typename results_t, typename rec_t> struct analyzer_t {
   }
 
   auto
-  push_task(const auto file_id, const auto &chunk) {
+  push_task(const auto &task) {
     std::unique_lock task_queue_lock(task_queue_mtx);
-    task_queue.emplace(file_id, chunk.first, chunk.second);
+    task_queue.emplace(task);
     task_queue_lock.unlock();
     task_queue_cv.notify_one();
   }
 
   [[nodiscard]] auto
-  pop_task(auto &task_queue_lock) {
+  pop_task_and_release_lock(auto &task_queue_lock) {
     const auto task = std::move(task_queue.front());
     task_queue.pop();
     task_queue_lock.unlock();
@@ -167,7 +272,7 @@ template <typename results_t, typename rec_t> struct analyzer_t {
   }
 
   [[nodiscard]] auto
-  pop_file(auto &file_queue_lock) {
+  pop_file_and_release_lock(auto &file_queue_lock) {
     const auto file_id = file_queue.front();
     file_queue.pop();
     file_queue_lock.unlock();
