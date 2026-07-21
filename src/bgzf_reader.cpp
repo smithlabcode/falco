614618240c63930f38b5ad17def017e7a064ebb3
Andrew D Smith <andrewds@usc.edu>
2026-07-20 19:55:42 -0700

    src/bgzf_reader.hcpp: adding a class to represent a reader for bgzf files that can read the underlying file, generate decompression tasks (in the form of bgzf_blocks) and hold the data those blocks come from and will be inflated into

---
+++ src/bgzf_reader.cpp
@@ -0,0 +1,86 @@
+// SPDX-License-Identifier: MIT; (c) 2026 Andrew D Smith
+
+#include "bgzf_reader.hpp"
+#include "bgzf_block.hpp"
+
+#include <cassert>
+#include <cstdint>
+#include <cstring>
+#include <mutex>
+#include <string>
+
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+[[nodiscard]] static constexpr auto
+get_unaligned_le32(const std::uint8_t *p) -> std::uint32_t {
+  return (static_cast<std::uint32_t>(p[3]) << 24) |
+         (static_cast<std::uint32_t>(p[2]) << 16) |
+         (static_cast<std::uint32_t>(p[1]) << 8) |
+         (static_cast<std::uint32_t>(p[0]) << 0);
+}
+
+[[nodiscard]] static constexpr auto
+get_isize(const auto *data, const auto data_size) {
+  static constexpr decltype(data_size) isize_size = 4;
+  assert(data_size > isize_size);
+  const auto u_data = reinterpret_cast<const std::uint8_t *>(data);
+  return data_size < isize_size
+           ? 0
+           : get_unaligned_le32(u_data + data_size - isize_size);
+}
+
+auto
+assign(gz_hdr &gz, auto *data) -> void {
+  std::memcpy(reinterpret_cast<std::uint8_t *>(&gz), data, gzip_header_size);
+}
+
+bgzf_reader::bgzf_reader(const std::string &filename,
+                         const std::int32_t buf_size) :
+  fp(std::fopen(std::data(filename), "r"), &std::fclose), buf_size{buf_size},
+  inbuf(buf_size), outbuf(buf_size), next_in{std::data(inbuf)},
+  end_in{std::data(inbuf)}, next_out{std::data(outbuf)},
+  end_out{std::data(outbuf) + buf_size} {}
+
+[[nodiscard]] auto
+bgzf_reader::read_data() -> bool {
+  if (std::feof(fp.get()))
+    return false;
+  const auto unused_bytes = std::distance(next_in, end_in);
+  std::copy(next_in, end_in, std::data(inbuf));
+  next_in = std::data(inbuf);
+  end_in = next_in + unused_bytes;
+  const std::int32_t avail_in =
+    buf_size - std::distance(std::data(inbuf), end_in);
+  const auto r = std::fread(end_in, 1, avail_in, fp.get());
+  if (std::ferror(fp.get()))
+    throw std::system_error(std::make_error_code(std::errc(errno)),
+                            "failed reading input");
+  end_in += r;  // will usually be end of inbuf
+  return r > 0;
+}
+
+[[nodiscard]] auto
+bgzf_reader::get_decomp_task() -> bgzf_block_t {
+  // ADS: need to ensure each constructed bgzf_block_t has an out_itr set and
+  // not to nullptr
+  if (std::distance(next_out, end_out) < max_bgzf_block_size)
+    return bgzf_block_t{};
+  if (std::distance(next_in, end_in) < gzip_header_size && !read_data())
+    return bgzf_block_t{};
+  assign(gh, next_in);
+  assert(gh.check_magic());
+  next_in += gzip_header_size;
+  const auto body_size =
+    (static_cast<std::uint32_t>(gh.size) + 1) - gzip_header_size;
+  if (std::distance(next_in, end_in) < body_size && !read_data())
+    return bgzf_block_t{};
+  const auto uncompressed_size = get_isize(next_in, body_size);
+  std::memcpy(next_out, next_in, body_size);
+  bgzf_block_t task(uncompressed_size, nullptr, next_out);
+  next_in += body_size;
+  next_out += max_bgzf_block_size;
+  return task;
+}
---
+++ src/bgzf_reader.hpp
@@ -0,0 +1,72 @@
+// SPDX-License-Identifier: MIT; (c) 2026 Andrew D Smith
+
+#ifndef SRC_BGZF_READER_HPP_
+#define SRC_BGZF_READER_HPP_
+
+#include "bgzf_block.hpp"
+
+#include <cstdint>
+#include <memory>
+#include <mutex>
+#include <string>
+#include <vector>
+
+static constexpr auto gzip_header_size = 18;
+
+struct gz_hdr {
+  static constexpr auto magic1 = 0x1F;
+  static constexpr auto magic2 = 0x8B;
+
+  std::uint8_t id1{};       // 0
+  std::uint8_t id2{};       // 1
+  std::uint8_t cm_eight{};  // 2
+  std::uint8_t flg{};       // 3
+  std::uint32_t mtime{};    // 7 [4]
+  std::uint8_t xfl{};       // 8
+  std::uint8_t os{};        // 9 [2]
+  std::uint16_t xlen{};     // 11
+  char b{};                 // 12
+  char c{};                 // 13
+  std::uint16_t two{};      // 14 [2]
+  std::uint16_t size{};     // 16 [4]
+
+  [[nodiscard]] auto
+  check_magic() const -> bool {
+    return id1 == magic1 && id2 == magic2;  // ADS: check b and c also
+  }
+};
+
+// reads data and provides serialized compressed chunks for deflation
+class bgzf_reader {
+private:
+  std::unique_ptr<std::FILE, int (*)(std::FILE *)> fp;
+  gz_hdr gh;
+  std::int32_t buf_size{};
+  std::vector<char> inbuf;
+  std::vector<char> outbuf;
+  char *next_in{};
+  char *end_in{};
+  char *next_out{};
+  char *end_out{};
+
+public:
+  bgzf_reader(const std::string &filename, const std::int32_t buf_size);
+
+  [[nodiscard]] auto
+  get_decomp_task() -> bgzf_block_t;
+
+  [[nodiscard]] auto
+  read_data() -> bool;
+
+  [[nodiscard]] auto
+  reset() {
+    next_out = std::data(outbuf);
+  }
+
+  [[nodiscard]] auto
+  has_out() {
+    return std::distance(next_out, end_out) >= max_bgzf_block_size;
+  }
+};
+
+#endif  // SRC_BGZF_READER_HPP_
