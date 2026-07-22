114ac622320da6712a581bdcb976326b10a46e12
Andrew D Smith <andrewds@usc.edu>
2026-07-22 16:23:18 -0700

    src/bgzf_block.hpp: moving the definition of bgzf_chunks_t here

--- src/bgzf_block.hpp
+++ src/bgzf_block.hpp
@@ -4,10 +4,14 @@
 #define SRC_BGZF_BLOCK_HPP_
 
 #include <cstdint>
+#include <vector>
 
 static constexpr auto max_bgzf_block_size = 65536;
 
 struct bgzf_block_t {
+  // ADS: because out_itr and in_itr reference separate blocks, either we need
+  // to track one of the sizes or we would have to add an end iterator for one
+  // of them.
   std::int32_t size{};
   char *out_itr{};
   char *in_itr{};
@@ -23,13 +27,15 @@ struct bgzf_block_t {
   auto operator=(bgzf_block_t &&src) noexcept -> bgzf_block_t & = default;
 
   [[nodiscard]] auto data() const -> const char * { return in_itr; }
-  [[nodiscard]] operator bool() const { return size > 0; }
   [[nodiscard]] auto operator<=>(const bgzf_block_t &other) const = default;
+  operator bool() const { return size > 0; }
   // clang-format on
 
   auto
   decompress() -> void;
 };
+
+using bgzf_chunks_t = std::vector<bgzf_block_t>;
 
 auto
 decompress(auto &&x) -> void {  // free function
