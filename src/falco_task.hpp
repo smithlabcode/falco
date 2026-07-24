6e1c734890bf6558e520a1db980e7df4961c9f67
Andrew D Smith <andrewds@usc.edu>
2026-07-23 20:00:45 -0700

    src/falco_task.hpp: adding this file to define the task variant for the task queue allowing it be easily visible where needed

---
+++ src/falco_task.hpp
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT; Copyright 2026 Andrew D Smith
+
+#ifndef SRC_FALCO_TASK_HPP_
+#define SRC_FALCO_TASK_HPP_
+
+#include "bamrec.hpp"
+#include "bgzf_block.hpp"
+#include "fqrec.hpp"
+
+#include <variant>
+
+using task_t =
+  std::variant<std::monostate, fq_task_t, bam_task_t, bgzf_block_t>;
+
+#endif  // SRC_FALCO_TASK_HPP_
