4637dce9fadcb77189c05cae7decdacf6d5b164a
Andrew D Smith <andrewds@usc.edu>
2026-07-27 09:52:36 -0700

    src/fastq_file.cpp: removing the file-local estimate_read_lengths and using estimate_read_lengths_fastq_chunk in falco_utils

--- src/fastq_file.cpp
+++ src/fastq_file.cpp
@@ -14,32 +14,10 @@
 #include <system_error>
 #include <vector>
 
-static constexpr auto fastq_lines_per_read = 4;
-
-[[nodiscard]] static auto
-estimate_read_length(const auto &data, const auto n) {
-  assert(n >= 1);
-  const auto valid = [](const auto c) {
-    return c == 'A' || c == 'C' || c == 'G' || c == 'T' || c == 'N';
-  };
-  std::vector<std::int64_t> lines;
-  for (auto i = 0u; i + 1 < n; ++i)
-    if (data[i] == '\n')
-      lines.push_back(i + 1);
-  if (std::size(lines) < fastq_lines_per_read)
-    return 1ul;
-  auto total = 0ul;
-  for (const auto l : lines | std::views::adjacent<fastq_lines_per_read - 1>)
-    if (data[std::get<0>(l)] == '@' && data[std::get<2>(l)] == '+' &&
-        valid(data[std::get<1>(l)]))
-      // cppcheck-suppress useStlAlgorithm
-      total += (std::get<2>(l) - std::get<1>(l)) - 1;
-  return total / (std::size(lines) / fastq_lines_per_read);
-}
-
 [[nodiscard]] auto
 estimate_n_reads_fastq(const std::string &filename)
   -> std::tuple<std::uint64_t, std::uint64_t, std::int64_t> {
+  static constexpr auto fastq_lines_per_read = 4;
   static constexpr auto n_parts = 10;
   static constexpr auto max_part_size = 1024 * 1024;
   static const auto page_mask = ~(sysconf(_SC_PAGESIZE) - 1);
@@ -67,7 +45,7 @@ estimate_n_reads_fastq(const std::string &filename)
                               "failed to mmap file");
     // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
     total_newlines += std::ranges::count(data, data + part_size, '\n');
-    read_len_est += estimate_read_length(data, part_size);
+    read_len_est += estimate_read_length_fastq_chunk(data, part_size);
 
     if (munmap(static_cast<void *>(data), part_size))
       throw std::system_error(std::make_error_code(std::errc(errno)),
