e9507a8a0be51c631cbf1fa389a83db261c3d915
Andrew D Smith <andrewds@usc.edu>
2026-07-27 16:59:47 -0700

    src/samrec.hcpp: adding samrec with the same data and interpreation (almost) as bamrec but which parses differently

---
+++ src/samrec.cpp
@@ -0,0 +1,104 @@
+// SPDX-License-Identifier: MIT; Copyright 2026 Andrew D Smith
+
+#include "samrec.hpp"
+
+#include <algorithm>
+#include <charconv>
+#include <iterator>
+
+#ifdef BAM_FREVERSE
+#undef BAM_FREVERSE
+#endif
+
+#ifdef bam_is_rev
+#undef bam_is_rev
+#endif
+
+[[nodiscard]] auto
+samrec::get_next(samrec::pos_t &cursor, const samrec::pos_t end_itr,
+                 samrec &rec) -> bool {
+  static constexpr auto n_fields_to_skip = 7;
+  constexpr auto complem = [](const auto x) {
+    return "TNGNNNCNNNNNNNNNNNNA"[x - 'A'];
+  };
+  static constexpr auto bam_is_rev = [](const auto flag) -> bool {
+    static constexpr auto BAM_FREVERSE = 16;
+    return (flag & BAM_FREVERSE) != 0;
+  };
+
+  auto itr = cursor;
+
+  const auto next_delim = [end_itr](auto &itr) {
+    itr = std::find(itr, end_itr, '\t');
+  };
+
+  // get the read name
+  const auto name_itr = itr;
+  next_delim(itr);
+  rec.name_len = std::distance(name_itr, itr);
+  if (itr++ == end_itr)
+    return false;
+
+  // get the flag (for revcomp)
+  int flag{};
+  const auto [flag_end, ec] = std::from_chars(itr, end_itr, flag);
+  if (ec != std::errc{})
+    return false;
+  itr += std::distance(itr, const_cast<samrec::pos_t>(flag_end));
+  if (itr++ == end_itr)
+    return false;
+
+  // skip fields
+  for (auto i = 0; i < n_fields_to_skip; ++i) {
+    next_delim(itr);
+    if (itr++ == end_itr)
+      return false;
+  }
+
+  // get the sequence
+  const auto seq_itr = itr;
+  next_delim(itr);
+  rec.seq_len = std::distance(seq_itr, itr);
+  if (itr++ == end_itr)
+    return false;
+
+  // get the quality scores
+  const auto qual_itr = itr;
+  next_delim(itr);
+  const auto qual_len = std::distance(qual_itr, itr);
+  if (itr++ == end_itr)
+    return false;
+
+  if (*qual_itr != qual_missing_symbol && qual_len != rec.seq_len)
+    return false;
+
+  const auto rec_size = rec.name_len + 2 * rec.seq_len;
+  if (std::size(rec.buffer) < rec_size)
+    rec.buffer.resize(rec_size);
+
+  auto buf_beg = std::begin(rec.buffer);
+  std::copy_n(name_itr, rec.name_len, buf_beg);
+  if (bam_is_rev(flag)) {
+    std::reverse_copy(seq_itr, seq_itr + rec.seq_len, buf_beg + rec.name_len);
+    auto buf_seq_itr = buf_beg + rec.name_len;
+    std::transform(buf_seq_itr, buf_seq_itr + rec.seq_len, buf_seq_itr,
+                   complem);
+    std::reverse_copy(qual_itr, qual_itr + rec.seq_len,
+                      buf_beg + rec.name_len + rec.seq_len);
+  }
+  else {
+    std::copy(seq_itr, seq_itr + rec.seq_len, buf_beg + rec.name_len);
+    std::copy(qual_itr, qual_itr + rec.seq_len,
+              buf_beg + rec.name_len + rec.seq_len);
+  }
+  if (*qual_itr == qual_missing_symbol)  // could use to eliminate copy
+    buf_beg[rec.name_len + rec.seq_len] = qual_missing_code;
+
+  itr = std::find(itr, end_itr, '\n');
+  if (*itr++ != '\n')
+    return false;
+
+  cursor = itr;
+
+  return true;
+}
---
+++ src/samrec.hpp
@@ -0,0 +1,141 @@
+// SPDX-License-Identifier: MIT; Copyright 2026 Andrew D Smith
+
+#ifndef SRC_SAMREC_HPP_
+#define SRC_SAMREC_HPP_
+
+#include <algorithm>
+#include <array>
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <filesystem>
+#include <format>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <memory>
+#include <ranges>
+#include <stdexcept>
+#include <string>
+#include <string_view>
+#include <utility>  // IWYU pragma: keep
+#include <variant>
+#include <vector>
+
+#ifdef bam_is_rev
+#undef bam_is_rev
+#endif
+
+#ifdef BAM_FREVERSE
+#undef BAM_FREVERSE
+#endif
+
+class samrec {
+public:
+  using pos_t = char *;
+
+  static constexpr auto qual_missing_symbol = '*';  // from SAMv1.pdf
+  static constexpr auto qual_missing_code = 0xff;   // from sam.c
+
+  std::vector<char> buffer;
+  std::uint32_t name_len{};
+  std::uint32_t seq_len{};
+
+public:
+  // clang-format off
+  friend constexpr auto get_name(const samrec &);
+  friend constexpr auto get_name_end(const samrec &);
+  friend constexpr auto get_seq(const samrec &);
+  friend constexpr auto get_seq_end(const samrec &);
+  friend constexpr auto get_seq_size(const samrec &);
+  friend constexpr auto get_qual(const samrec &);
+  friend constexpr auto get_qual_end(const samrec &);
+  friend constexpr auto get_qual_size(const samrec &);
+  // clang-format on
+
+  // clang-format off
+  samrec() = default;
+  ~samrec() = default;
+  samrec(const samrec &) = delete;
+  auto operator=(const samrec &) -> samrec & = delete;
+  auto operator=(samrec &&) noexcept -> samrec & = delete;
+  samrec(samrec &&) noexcept = delete;
+  // clang-format on
+
+  [[nodiscard]] auto
+  to_string() const {
+    return std::format(
+      "@{}\n{}\n+\n{}",
+      std::string(std::data(buffer), std::data(buffer) + name_len),
+      std::string(std::data(buffer) + name_len,
+                  std::data(buffer) + name_len + seq_len),
+      std::string(std::data(buffer) + name_len + seq_len,
+                  std::data(buffer) + name_len + seq_len + seq_len));
+  }
+
+  operator bool() const { return name_len != 0; }
+
+  [[nodiscard]] static auto
+  get_next(samrec::pos_t &cursor, const samrec::pos_t end_itr,
+           samrec &rec) -> bool;
+
+  [[nodiscard]] static auto
+  find_end_pos(pos_t itr, const pos_t end) -> samrec::pos_t;
+};
+
+// NOLINTBEGIN(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+[[nodiscard]] inline constexpr auto
+get_name(const samrec &rec) {
+  return std::data(rec.buffer);
+}
+
+[[nodiscard]] inline constexpr auto
+get_name_end(const samrec &rec) {
+  return get_name(rec) + rec.name_len;
+}
+
+[[nodiscard]] inline constexpr auto
+get_seq(const samrec &rec) {
+  return get_name_end(rec);
+}
+
+[[nodiscard]] inline constexpr auto
+get_seq_end(const samrec &rec) {
+  return get_seq(rec) + rec.seq_len;
+}
+
+[[nodiscard]] inline constexpr auto
+get_seq_size(const samrec &rec) {
+  return rec.seq_len;
+}
+
+[[nodiscard]] inline constexpr auto
+get_qual(const samrec &rec) {
+  return get_seq_end(rec);
+}
+
+[[nodiscard]] inline constexpr auto
+get_qual_end(const samrec &rec) {
+  return get_qual(rec) + rec.seq_len;
+}
+
+[[nodiscard]] inline constexpr auto
+get_qual_size(const samrec &rec) {
+  return get_seq_size(rec);
+}
+// NOLINTEND(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+
+using sam_chunks_t = std::vector<std::pair<samrec::pos_t, samrec::pos_t>>;
+
+struct sam_task_t {
+  samrec::pos_t beg{};
+  samrec::pos_t end{};
+};
+
+[[nodiscard]] inline auto
+get_next(auto &itr, const auto end, samrec &rec) -> bool {
+  return samrec::get_next(itr, end, rec);
+}
+
+#endif  // SRC_SAMREC_HPP_
