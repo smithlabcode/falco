691d94009f0caac5ff29b3ff9623e54555ffbe67
Andrew D Smith <andrewds@usc.edu>
2026-07-27 17:10:36 -0700

    src/falco_task.hpp: adding sam_task_t to the task_t variant

--- src/falco_task.hpp
+++ src/falco_task.hpp
@@ -6,10 +6,11 @@
 #include "bamrec.hpp"
 #include "bgzf_block.hpp"
 #include "fqrec.hpp"
+#include "samrec.hpp"
 
 #include <variant>
 
 using task_t =
-  std::variant<std::monostate, fq_task_t, bam_task_t, bgzf_block_t>;
+  std::variant<std::monostate, fq_task_t, bam_task_t, sam_task_t, bgzf_block_t>;
 
 #endif  // SRC_FALCO_TASK_HPP_
