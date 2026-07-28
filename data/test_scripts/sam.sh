819188119aad76c979014628148bac86e21d2d10
Andrew D Smith <andrewds@usc.edu>
2026-07-27 17:00:15 -0700

    data/test_scripts/sam.sh: adding a test for SAM format

---
+++ data/test_scripts/sam.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: MIT; Copyright 2026 Andrew D Smith
+
+# Most basic test on regular SAM file
+
+if [[ ! -e $(type -P samtools) ]]; then
+    echo "samtools not found; skipping remaining tests";
+    exit 77;
+fi
+
+prog=./falco
+infile1=test_data/bam_1.bam
+infile2=sam_1.sam
+outdir=sam_out
+if [[ -e "${infile1}" ]]; then
+    mkdir -p ${outdir}
+    samtools view -h -o ${infile2} ${infile1}
+    ${prog} -o ${outdir} ${infile2}
+    x=$(md5sum --ignore-missing -c test_data/md5sum.txt | \
+            grep "${outdir}" | \
+            grep -c "OK$")
+    if [[ "${x}" != "1" ]]; then
+        exit 1;
+    fi
+    rm -r ${outdir}
+else
+    echo "${infile1} not found; skipping remaining tests";
+    exit 77;
+fi
