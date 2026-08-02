dc6c744a0d570d5db69b5ebaf3b824b8820de8cd
Andrew D Smith <andrewds@usc.edu>
2026-08-01 19:54:41 -0700

    src/falco_analyzer.hcpp: removing the analyzer_t class in favor of just one function

--- src/falco_analyzer.hpp
+++ src/falco_analyzer.hpp
@@ -3,29 +3,18 @@
 #ifndef SRC_FALCO_ANALYZER_HPP_
 #define SRC_FALCO_ANALYZER_HPP_
 
+#include "reads_file.hpp"  // IWYU pragma: keep
 #include "results_collector.hpp"
-#include "task_queue.hpp"
 
-#include <atomic>
 #include <cstdint>
-#include <format>
 #include <vector>
 
-class reads_file_t;
 class run_mode;
 struct file_info;
 
-struct analyzer_t {
-  task_queue tq;
-  // ADS: likely false sharing between n_tasks members
-  std::vector<std::atomic_int32_t> n_tasks;  // count of submitted tasks by file
-  std::atomic_uint32_t n_active_files{};
-  std::vector<std::vector<results_collector>> results;  // n_threads x n_tiles
-  analyzer_t(const std::uint32_t n_threads,             //
-             const run_mode &mode,                      //
-             const std::vector<file_info> &infos,       //
-             std::vector<reads_file_t> &reads_files     //
-  );
-};
+[[nodiscard]] auto
+analyze(const std::uint32_t n_threads, const run_mode &mode,
+        std::vector<file_info> &infos, std::vector<reads_file_t> reads_files)
+  -> std::vector<results_collector>;
 
 #endif  // SRC_FALCO_ANALYZER_HPP_
