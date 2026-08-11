365b8a7d708077ec1c03633cfb365965a23e61c5
Andrew D Smith <andrewds@usc.edu>
2026-08-11 12:27:17 -0700

    src/results_collector.hpp: adding a mode to initialize a results_collector when duplication words to be counted need to be initialized for original dups

--- src/results_collector.hpp
+++ src/results_collector.hpp
@@ -62,6 +62,16 @@ struct alignas(assumed_page_size) results_collector {
     am.resize(updated_length);
   }
 
+#ifdef ORIGINAL_DUPS
+  auto
+  init(const run_mode &mode, const auto &info, const auto &dups_init) {
+    dr.initialize(mode, info, dups_init);
+    do_tiles = mode.do_tiles() && info.has_tiles;
+    do_kmers = mode.do_kmers();
+    if (do_tiles)
+      tp.init(info);
+  }
+#else   // NOT ORIGINAL_DUPS
   auto
   init(const run_mode &mode, const auto &info) {
     dr.initialize(mode, info);
@@ -70,6 +80,7 @@ struct alignas(assumed_page_size) results_collector {
     if (do_tiles)
       tp.init(info);
   }
+#endif  // ORIGINAL_DUPS
 
   auto
   adjust_base_counts_for_ns() -> void {
