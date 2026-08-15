fb4b34ca96877715da3b604f3a486ec7f471265c
Andrew D Smith <andrewds@usc.edu>
2026-08-15 15:23:13 -0700

    data/test_scripts/preseq.sh: adding a test for --preseq mode

---
+++ data/test_scripts/preseq.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+# SPDX-License-Identifier: MIT
+
+prog=./falco
+infile=test_data/bam_1.bam
+outdir=preseq_out
+if [[ -e "${infile}" ]]; then
+    ${prog} --preseq -o ${outdir} ${infile}
+    x=$(md5sum --ignore-missing -c test_data/md5sum.txt | \
+            grep "${outdir}" | \
+            grep -c "OK$")
+    if [[ "${x}" != "1" ]]; then
+        exit 1;
+    fi
+    rm -r ${outdir}
+else
+    echo "${infile} not found; skipping remaining tests";
+    exit 77;
+fi
