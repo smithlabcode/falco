cbe4507306c447519191e9b45e8b86416f22ed8e
Andrew D Smith <andrewds@usc.edu>
2026-08-23 18:20:51 -0700

    src/fastq_file.hcpp: moving most of the implementation into the source file

--- src/fastq_file.cpp
+++ src/fastq_file.cpp
@@ -2,15 +2,24 @@
 
 #include "fastq_file.hpp"
 #include "falco_utils.hpp"
+#include "fqrec.hpp"
+#include "task_queue.hpp"
 
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 #include <unistd.h>
 
+#include <cassert>
 #include <cstdint>
+#include <cstdlib>
+#include <ranges>
+#include <span>
 #include <string>
 #include <system_error>
+#include <type_traits>
+#include <utility>
+#include <vector>
 
 [[nodiscard]] auto
 estimate_n_reads_fastq(const std::string &filename)
@@ -27,34 +36,117 @@ estimate_n_reads_fastq(const std::string &filename)
   fstat(fd, &buf);
   const auto filesize = buf.st_size;
   if (filesize < n_parts)
-    return {0LU, 0LU, filesize};
+    return {{}, {}, filesize};
 
   const auto part_size =
     filesize < n_parts * max_part_size ? filesize / n_parts : max_part_size;
 
   auto total_newlines = 0ul;
-  auto read_len_est = 0ul;
+  auto readlen_est = 0ul;
   for (auto i = 0; i < n_parts; ++i) {
     const auto offset = (i * part_size) & page_mask;
-    char *data = static_cast<char *>(
-      mmap(nullptr, part_size, PROT_READ, MAP_PRIVATE, fd, offset));
-    if (data == MAP_FAILED)
+    auto raw = mmap(nullptr, part_size, PROT_READ, MAP_PRIVATE, fd, offset);
+    if (raw == MAP_FAILED)
       throw std::system_error(std::make_error_code(std::errc(errno)),
                               "failed to mmap file");
-    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-    total_newlines += std::ranges::count(data, data + part_size, '\n');
-    read_len_est += estimate_read_length_fastq_chunk(data, part_size);
-
-    if (munmap(static_cast<void *>(data), part_size))
+    const auto data = std::span(static_cast<char *>(raw), part_size);
+    total_newlines += std::ranges::count(data, '\n');
+    readlen_est += estimate_read_length_fastq_chunk(std::data(data), part_size);
+    if (munmap(raw, part_size))
       throw std::system_error(std::make_error_code(std::errc(errno)),
                               "failed to unmap memory");
   }
   close(fd);
 
-  read_len_est /= n_parts;
+  readlen_est /= n_parts;
   const auto n_reads_est =
     as_frac(total_newlines, fastq_lines_per_read) *
     as_frac(static_cast<double>(filesize), (part_size * n_parts));
 
-  return {static_cast<std::uint64_t>(n_reads_est), read_len_est, filesize};
+  return {static_cast<std::uint64_t>(n_reads_est), readlen_est, filesize};
+}
+
+static inline auto
+mmap_fastq(const int fd, const std::int64_t offset, const std::int64_t length,
+           auto &data) {
+  static constexpr auto prot = PROT_READ;
+  static constexpr auto flags = MAP_PRIVATE;
+  data = static_cast<char *>(mmap(nullptr, length, prot, flags, fd, offset));
+  if (data == MAP_FAILED)
+    throw std::system_error(std::make_error_code(std::errc(errno)),
+                            "failed to mmap file");
+}
+
+static inline auto
+cleanup_mmap_fastq(auto &buffer, std::int64_t &buffer_size) {
+  if (buffer == nullptr)
+    return;
+  munmap(static_cast<void *>(buffer), buffer_size);
+  buffer = nullptr;
+  buffer_size = 0;
+}
+
+auto
+fastq_file::reset() -> void {
+  cleanup_mmap_fastq(buffer, buffer_size);
+}
+
+auto
+fastq_file::load_next() -> void {
+  // memory mapped data is page aligned but the data we need is not
+  static const auto page_mask = sysconf(_SC_PAGESIZE) - 1;
+  std::tie(start_in_file, cursor) = [&] {
+    const auto pos_in_file = start_in_file + cursor;
+    return std::tuple(pos_in_file & (~page_mask), pos_in_file & page_mask);
+  }();
+  stop_in_file = std::min(filesize, start_in_file + target_buffer_size);
+  if (buffer_size > 0)
+    cleanup_mmap_fastq(buffer, buffer_size);
+  if (start_in_file < stop_in_file) {  // this exist for empty files
+    buffer_size = stop_in_file - start_in_file;
+    mmap_fastq(fd, start_in_file, buffer_size, buffer);
+  }
+}
+
+auto
+fastq_file::get_chunks(const std::int64_t n_chunks, const std::int32_t file_id,
+                       task_queue &tq, std::atomic_int32_t &n_tasks) -> void {
+  static constexpr auto rec_lines = 4;  // FASTQ
+  assert(n_chunks > 0);
+  const auto buf = std::span(buffer, buffer_size);
+  // clang-format off
+  const auto not_read_start = [&](const auto p) {
+    // ADS: could get confused if '+' lines have full name info
+    return buf[p] != '@' || (p > 0 && buf[p - 1] != '\n') ||
+           (p > 2 && buf[p - 2] == '+' && buf[p - 3] == '\n');
+  };
+  const auto fwd_to_read_start = [&](auto pos) {
+    if (pos == 0) return pos;
+    while (pos < buffer_size && not_read_start(pos)) ++pos;
+    return pos;
+  };
+  const auto rev_to_read_start = [&](auto pos) {
+    while (pos > 0 && (pos == buffer_size || not_read_start(pos))) --pos;
+    return pos;
+  };
+  // clang-format on
+  const auto n_bytes_available = buffer_size - cursor;
+  std::int64_t start_pos = cursor;
+  const auto [chunk_size, remainder] = std::div(n_bytes_available, n_chunks);
+  std::vector<std::pair<std::int64_t, std::int64_t>> chunks(n_chunks);
+  std::int64_t chunk_end{start_pos};
+  for (const auto chunk_idx : std::views::iota(0, n_chunks)) {
+    const auto chunk_beg = fwd_to_read_start(start_pos);
+    const auto stop_pos = start_pos + chunk_size + (chunk_idx < remainder);
+    chunk_end = fwd_to_read_start(stop_pos);
+    if (chunk_idx + 1 == n_chunks)
+      if (const auto prev = rev_to_read_start(chunk_end);
+          std::count(std::cbegin(buf) + prev, std::cend(buf), '\n') < rec_lines)
+        chunk_end = prev;
+    ++n_tasks;
+    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
+    tq.push(file_id, fq_task_t(buffer + chunk_beg, buffer + chunk_end));
+    start_pos = stop_pos;
+  }
+  cursor = chunk_end;
 }
--- src/fastq_file.hpp
+++ src/fastq_file.hpp
@@ -3,70 +3,38 @@
 #ifndef SRC_FASTQ_FILE_HPP_
 #define SRC_FASTQ_FILE_HPP_
 
-#include "fqrec.hpp"
-#include "task_queue.hpp"
-
 #include <fcntl.h>
-#include <sys/mman.h>
 #include <unistd.h>
 
 #include <algorithm>
 #include <atomic>
-#include <cassert>
 #include <cerrno>
 #include <cstdint>
-#include <cstdlib>
 #include <filesystem>
 #include <iterator>
-#include <ranges>
 #include <string>
 #include <system_error>
 #include <tuple>
-#include <utility>
 #include <variant>
-#include <vector>
 
-struct fastq_buffer {
-  char *data{};       // not necessarily owned
-  std::int64_t sz{};  // slight redundancy with vars containing classes
-};
-
-static inline auto
-mmap_fastq(const int fd, const std::int64_t start_pos_in_file,
-           const std::int64_t stop_pos_in_file, fastq_buffer &buf) {
-  const auto n_bytes = stop_pos_in_file - start_pos_in_file;
-  char *data = static_cast<char *>(
-    mmap(nullptr, n_bytes, PROT_READ, MAP_PRIVATE, fd, start_pos_in_file));
-  if (data == MAP_FAILED)
-    throw std::system_error(std::make_error_code(std::errc(errno)),
-                            "failed to mmap file");
-  buf.data = data;
-  buf.sz = n_bytes;
-}
-
-static inline auto
-cleanup_mmap_fastq(fastq_buffer &buf) {
-  if (buf.data == nullptr)
-    return;
-  [[maybe_unused]] const auto r = munmap(static_cast<void *>(buf.data), buf.sz);
-  buf.data = nullptr;
-  buf.sz = 0;
-}
+struct task_queue;
 
 struct fastq_file {
-  static constexpr auto min_buf_size = 64 * 1024;
-  std::int64_t buf_size{};
+  static constexpr auto min_buf_size = 65536;
+  std::int64_t target_buffer_size{};
   std::int64_t filesize{};
-  fastq_buffer buf{};
-  std::int64_t start_pos_in_file{};
-  std::int64_t stop_pos_in_file{};
+  char *buffer{};
+  std::int64_t buffer_size{};
+  std::int64_t start_in_file{};
+  std::int64_t stop_in_file{};
   std::int64_t cursor{};
   int fd{};
 
-  fastq_file(const std::string &filename, const std::int64_t buf_size_arg) :
-    buf_size{buf_size_arg + min_buf_size},
+  fastq_file(const std::string &filename,
+             const std::int64_t target_buffer_size) :
+    target_buffer_size{
+      std::max(target_buffer_size, static_cast<std::int64_t>(min_buf_size))},
     filesize{static_cast<std::int64_t>(std::filesystem::file_size(filename))},
-    stop_pos_in_file{buf_size_arg + min_buf_size},  // init to use as sentinel
     fd{open(std::data(filename), O_RDONLY, 0)} {
     if (fd < 0)
       throw std::system_error(std::make_error_code(std::errc(errno)),
@@ -80,99 +48,33 @@ struct fastq_file {
   // clang-format on
 
   fastq_file(fastq_file &&src) noexcept :
-    buf_size{src.buf_size},                    //
-    filesize{src.filesize},                    //
-    buf{src.buf},                              //
-    start_pos_in_file{src.start_pos_in_file},  //
-    stop_pos_in_file{src.stop_pos_in_file},    //
-    cursor{src.cursor},                        //
-    fd{dup(src.fd)}                            // <- LOOK
+    target_buffer_size{src.target_buffer_size},  //
+    filesize{src.filesize},                      //
+    buffer{src.buffer},                          //
+    buffer_size{src.buffer_size},                //
+    start_in_file{src.start_in_file},            //
+    stop_in_file{src.stop_in_file},              //
+    cursor{src.cursor},                          //
+    fd{dup(src.fd)}                              // <- LOOK
   {}
 
   auto
-  reset() -> void {
-    if (buf.sz > 0)
-      cleanup_mmap_fastq(buf);
-  }
+  reset() -> void;
 
   ~fastq_file() {
     reset();
     close(fd);  // will always have been opened using a filename
   }
 
-  [[nodiscard]] operator bool() const {
-    return stop_pos_in_file - start_pos_in_file == buf_size;
-  }
+  operator bool() const { return stop_in_file != filesize; }
 
   auto
-  load_next() -> void {
-    // memory mapped data is page aligned but the data we need is not
-    static const auto page_mask = sysconf(_SC_PAGESIZE) - 1;
-    std::tie(start_pos_in_file, cursor) = [&] {
-      const auto pos_in_file = start_pos_in_file + cursor;
-      return std::tuple(pos_in_file & (~page_mask), pos_in_file & page_mask);
-    }();
-    stop_pos_in_file = std::min(filesize, start_pos_in_file + buf_size);
-    if (buf.sz > 0)
-      cleanup_mmap_fastq(buf);
-    if (start_pos_in_file < stop_pos_in_file)  // this exist for empty files
-      mmap_fastq(fd, start_pos_in_file, stop_pos_in_file, buf);
-  }
+  load_next() -> void;
 
-  [[nodiscard]] auto
-  get_chunks(const std::int64_t n_chunks) -> fq_chunks_t;
+  auto
+  get_chunks(const std::int64_t n_chunks, const std::int32_t file_id,
+             task_queue &tq, std::atomic_int32_t &n_tasks) -> void;
 };
-
-[[nodiscard]] static inline auto
-get_chunks_fastq_impl(auto &fq, const std::int64_t n_chunks) {
-  static constexpr auto rec_lines = 4;  // FASTQ
-  // clang-format off
-  const auto not_read_start = [](const auto s, const auto p) {
-    // ADS: could get confused if '+' lines have full name info
-    return s[p] != '@' || (p > 0 && s[p-1] != '\n') ||
-      (p > 2 && s[p-2] == '+' && s[p-3] == '\n');
-  };
-  const auto fwd_to_read_start = [&](const auto &buf, auto pos) {
-    if (pos == 0) return pos;
-    while (pos < buf.sz && not_read_start(buf.data, pos)) ++pos;
-    return pos;
-  };
-  const auto rev_to_read_start = [&](const auto &buf, auto pos) {
-    while (pos > 0 && (pos == buf.sz || not_read_start(buf.data, pos))) --pos;
-    return pos;
-  };
-  // clang-format on
-  const auto &buf = fq.buf;  // non-copyable
-  const auto n_bytes_available = buf.sz - fq.cursor;
-  const auto [chunk_size, remainder] = std::div(n_bytes_available, n_chunks);
-  std::vector<std::pair<std::int64_t, std::int64_t>> chunks(n_chunks);
-  std::int64_t start_pos = fq.cursor;
-  for (const auto chunk_idx : std::views::iota(0, n_chunks)) {
-    const auto chunk_beg = fwd_to_read_start(buf, start_pos);
-    const auto stop_pos = start_pos + chunk_size + (chunk_idx < remainder);
-    const auto chunk_end = fwd_to_read_start(buf, stop_pos);
-    chunks[chunk_idx] = {chunk_beg, chunk_end};
-    start_pos = stop_pos;
-  }
-  // make sure final chunk includes only full records
-  const auto prev_start = rev_to_read_start(buf, chunks.back().second);
-  // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-  if (std::count(buf.data + prev_start, buf.data + buf.sz, '\n') < rec_lines)
-    chunks.back().second = prev_start;
-  fq.cursor = chunks.back().second;
-  return chunks;
-}
-
-[[nodiscard]] inline auto
-fastq_file::get_chunks(const std::int64_t n_chunks) -> fq_chunks_t {
-  assert(n_chunks > 0);
-  const auto add_offset = [d = buf.data](const auto &x) {
-    // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-    return std::pair{d + x.first, d + x.second};
-  };
-  return get_chunks_fastq_impl(*this, n_chunks) |
-         std::views::transform(add_offset) | std::ranges::to<std::vector>();
-}
 
 [[nodiscard]] auto
 estimate_n_reads_fastq(const std::string &filename)
@@ -188,11 +90,7 @@ make_tasks(fastq_file &reads_file,        //
   const auto n_chunks = n_chunks_per_thread * n_threads;
   n_tasks = 1;  // for current task, which makes tasks
   reads_file.load_next();
-  auto chunks = reads_file.get_chunks(n_chunks);
-  for (const auto &chunk : chunks) {
-    ++n_tasks;  // ADS: increment before submitting
-    tq.push(file_id, fq_task_t(chunk.first, chunk.second));
-  }
+  reads_file.get_chunks(n_chunks, file_id, tq, n_tasks);
 }
 
 inline auto
