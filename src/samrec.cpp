c0db0d23d63e44a31867432ea628bb9acd31f0c4
Andrew D Smith <andrewds@usc.edu>
2026-08-23 17:30:59 -0700

    src/samrec.cpp: moved to_string here from samrec.hpp and changed the get_next function to work with iterators instead of pointers as samrec::pos_t

--- src/samrec.cpp
+++ src/samrec.cpp
@@ -1,12 +1,14 @@
 // SPDX-License-Identifier: MIT; Copyright 2026 Andrew D Smith
 
 #include "samrec.hpp"
+#include "falco_utils.hpp"
 #include "quality_score.hpp"
 
 #include <algorithm>
 #include <cassert>
-#include <charconv>
+#include <format>
 #include <iterator>
+#include <span>
 #include <stdexcept>
 #include <system_error>
 
@@ -19,8 +21,19 @@
 #endif
 
 [[nodiscard]] auto
-samrec::get_next(samrec::pos_t &cursor, const samrec::pos_t end_itr,
-                 samrec &rec) -> bool {
+samrec::to_string() const -> std::string {
+  const auto buffer_s = std::span(std::cbegin(buffer), std::cend(buffer));
+  const auto name_s = buffer_s.subspan(0, name_len);
+  const auto seq_s = buffer_s.subspan(name_len, seq_len);
+  const auto qual_s = buffer_s.subspan(name_len + seq_len, seq_len);
+  return std::format("@{}\n{}\n+\n{}",  //
+                     std::string(std::cbegin(name_s), std::cend(name_s)),
+                     std::string(std::cbegin(seq_s), std::cend(seq_s)),
+                     std::string(std::cbegin(qual_s), std::cend(qual_s)));
+}
+
+[[nodiscard]] auto
+samrec::get_next(pos_t &cursor, const pos_t end_itr, samrec &rec) -> bool {
   static constexpr auto n_fields_to_skip = 7;
   constexpr auto complem = [](const auto x) {
     return "TNGNNNCNNNNNNNNNNNNA"[x - 'A'];
@@ -30,11 +43,11 @@ samrec::get_next(samrec::pos_t &cursor, const samrec::pos_t end_itr,
     return (flag & BAM_FREVERSE) != 0;
   };
 
-  auto itr = cursor;
-
   const auto next_delim = [end_itr](auto &itr) {
     itr = std::find(itr, end_itr, '\t');
   };
+
+  auto itr = cursor;
 
   // get the read name
   const auto name_itr = itr;
@@ -44,15 +57,12 @@ samrec::get_next(samrec::pos_t &cursor, const samrec::pos_t end_itr,
     return false;
   rec.name_len = name_len;
 
-  // NOLINTBEGIN(cppcoreguidelines-pro-bounds-pointer-arithmetic)
-
   // get the flag (for revcomp)
   int flag{};
-  auto [flag_end, ec] = std::from_chars(itr, end_itr, flag);
+  auto [flag_end, ec] = falco::from_chars(itr, end_itr, flag);
   if (ec != std::errc{})
     return false;
-  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-const-cast)
-  itr += std::distance(itr, const_cast<samrec::pos_t>(flag_end));
+  itr += std::distance(itr, flag_end);
   if (itr++ == end_itr)
     return false;
 
@@ -80,13 +90,13 @@ samrec::get_next(samrec::pos_t &cursor, const samrec::pos_t end_itr,
 
   if (qual_len != rec.seq_len)
     throw std::runtime_error("quality scores invalid for record: " +
-                             std::string(name_itr, name_len));
+                             std::string(name_itr, name_itr + name_len));
 
   const auto rec_size = rec.name_len + 2 * rec.seq_len;
   if (std::ssize(rec.buffer) < rec_size)
     rec.buffer.resize(rec_size);
 
-  auto out_name_itr = std::data(rec.buffer);
+  auto out_name_itr = std::begin(rec.buffer);
   auto out_seq_itr = out_name_itr + name_len;
   auto out_qual_itr = out_seq_itr + seq_len;
   std::copy_n(name_itr, name_len, out_name_itr);
@@ -109,8 +119,6 @@ samrec::get_next(samrec::pos_t &cursor, const samrec::pos_t end_itr,
     return false;
 
   cursor = itr;
-
-  // NOLINTEND(cppcoreguidelines-pro-bounds-pointer-arithmetic)
 
   return true;
 }
