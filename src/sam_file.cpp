e6ae0ac247fdd3204834f2f1ff0a1113f3c84c92
Andrew D Smith <andrewds@usc.edu>
2026-09-03 08:01:49 -0700

    src/sam_file.cpp and src/fastq_stdin.cpp: preventing compiler from complaints about mismatching types in std::div

--- src/sam_file.cpp
+++ src/sam_file.cpp
@@ -76,7 +76,7 @@ sam_file::get_chunks(const std::int64_t n_chunks,  //
     return p;
   };
   // clang-format on
-  const auto n_bytes_available = std::distance(beg_itr, end_itr);
+  const std::int64_t n_bytes_available = std::distance(beg_itr, end_itr);
   const auto [chunk_size, remainder] = std::div(n_bytes_available, n_chunks);
   assert(n_chunks > 0);
   auto start_itr = beg_itr;
